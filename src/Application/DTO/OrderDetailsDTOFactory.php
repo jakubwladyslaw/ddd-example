852beeb67ca89204b098a55199e6e63de3e01e0e
Jakub Żurawa <jakub.zurawa@lppsa.com>
2020-06-18 17:44:49 +0200

    fix OrderDetailsDTOFactory

--- src/Application/DTO/OrderDetailsDTOFactory.php
+++ src/Application/DTO/OrderDetailsDTOFactory.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Application\DTO;
 
+use App\Domain\Package;
+
 class OrderDetailsDTOFactory
 {
     public function createOrderDetailsDTO(Package $package): OrderDetailsDTO
